fba3c8686cbd9ddc86294a79d4e50635f1f0d9b3
Nicolas balao <nicolasbalao2@gmail.com>
2021-01-11 23:09:14 +0100

    manque reset

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var Return = function () {
   card.forEach((item) => {
     item.addEventListener("click", () => {
       let cardReturn = document.querySelectorAll(".return");
-      console.log(cardReturn.length);
+      // console.log(cardReturn.length);
       if (cardReturn.length == 2) {
         // pour retourner les cartes au bout de deux cartes retourner
-        console.log(cardReturn);
+        // console.log(cardReturn);
         cardReturn.forEach((item) => {
           item.classList.remove("return");
         });
@@ -18,12 +18,40 @@ var Return = function () {
 };
 
 var Random = function () {
-  const Choise = ["green", "yellow"];
+  const Choise = ["green", "yellow", "green", "yellow"];
   const indexCard = [0, 1, 2, 3];
-  let indexCardChoise = Math.floor(Math.random() * indexCard.length);
-  let indexChoise = Math.floor(Math.random() * Choise.length);
+  let indexCardShuffle = indexCard.sort(() => Math.random() - 0.5);
+  // console.log(indexCardShuffle);
 
-  card[indexCardChoise].classList.add(Choise[indexChoise]);
+  for (var i = 0; i < indexCardShuffle.length; i++) {
+    // console.log(i);
+    // console.log("salut");
+    card[indexCardShuffle[i]].classList.add(Choise[i]);
+  }
+
+  // card[indexCardChoise].classList.add(Choise[indexChoise]);
 };
+
+var chekWin = () => {
+  setInterval(() => {
+    let CardCheckReturn = document.querySelectorAll(".return");
+    let value1 = CardCheckReturn[0].classList[1];
+    let value2 = CardCheckReturn[1].classList[1];
+
+    if (value1 === value2) {
+      for (var i = 0; i < 2; i++) {
+        CardCheckReturn[i].classList.add("win");
+      }
+    }
+  }, 10);
+};
+
+var Reset = () => {
+  card.forEach((item) => {
+    item.className = "card";
+  });
+};
+
 Random();
+chekWin();
 Return();
