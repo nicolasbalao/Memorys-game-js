2fd42adc6b7c3ce037df69dd58a62671ea8c6f57
Nicolas balao <nicolasbalao2@gmail.com>
2021-01-13 21:40:33 +0100

    need assets

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const card = document.querySelectorAll(".card");
 
+////////////RETOURNER LA CARTE /////////
+
 var Return = function () {
   card.forEach((item) => {
     item.addEventListener("click", () => {
@@ -17,34 +19,72 @@ var Return = function () {
   });
 };
 
+//////MELANGER///////////////
+
 var Random = function () {
-  const Choise = ["green", "yellow", "green", "yellow"];
-  const indexCard = [0, 1, 2, 3];
+  const Choise = [
+    "green",
+    "yellow",
+    "green",
+    "yellow",
+    "black",
+    "black",
+    "orange",
+    "orange",
+    "red",
+    "red",
+    "rebeccapurple",
+    "rebeccapurple",
+    "violet",
+    "violet",
+    "brown",
+    "brown",
+  ];
+  let indexCard = [];
+  for (var i = 0; i < Choise.length; i++) {
+    indexCard.push(i);
+  }
+  console.log(indexCard);
   let indexCardShuffle = indexCard.sort(() => Math.random() - 0.5);
-  // console.log(indexCardShuffle);
 
   for (var i = 0; i < indexCardShuffle.length; i++) {
-    // console.log(i);
-    // console.log("salut");
     card[indexCardShuffle[i]].classList.add(Choise[i]);
   }
 
   // card[indexCardChoise].classList.add(Choise[indexChoise]);
 };
 
+///////////// ChekWin //////////////////
+
 var chekWin = () => {
   setInterval(() => {
     let CardCheckReturn = document.querySelectorAll(".return");
-    let value1 = CardCheckReturn[0].classList[1];
-    let value2 = CardCheckReturn[1].classList[1];
+    try {
+      let value1 = CardCheckReturn[0].classList[1];
+      let value2 = CardCheckReturn[1].classList[1];
 
-    if (value1 === value2) {
-      for (var i = 0; i < 2; i++) {
-        CardCheckReturn[i].classList.add("win");
+      if (value1 === value2) {
+        for (var i = 0; i < 2; i++) {
+          CardCheckReturn[i].classList.add("win");
+        }
       }
-    }
+    } catch (error) {}
   }, 10);
 };
+
+var Win = () => {
+  setInterval(() => {
+    let win = document.querySelectorAll(".win");
+
+    if (win.length == card.length) {
+      alert("you win");
+      clearInterval(Win);
+      Reset();
+      Random();
+    }
+  }, 50);
+};
+// Reset btn
 
 var Reset = () => {
   card.forEach((item) => {
@@ -54,4 +94,5 @@ var Reset = () => {
 
 Random();
 chekWin();
+Win();
 Return();
